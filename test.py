90f593daa0bcaa4490d986826a9ed7875b08c0d3
xfinnbar <>
2021-07-23 17:27:58 +0000

    Initial commit

---
+++ test.py
@@ -0,0 +1,23 @@
+from main import ScarletDB
+
+db = ScarletDB([])
+db.replit("data") # data is replit db the key that the database is stored in
+
+db.clear()
+
+db.insert_many([
+  {
+    "name": "AJ",
+    "pet": "Rabbit"
+  },
+  {
+    "name": "Jack",
+    "pet": "Dog"
+  }
+])
+
+db.update({"name": "Jack"}, {"name": "Tom"})
+
+print(db.get({
+  "pet": "Dog"
+}))
